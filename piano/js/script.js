fe3600352e09452f173656997afc60a7daccb784
rj7shakya <shakyarajad1@gmail.com>
2021-02-11 08:30:56 +0545

    auto play addition

--- piano/js/script.js
+++ piano/js/script.js
@@ -1,6 +1,9 @@
 let keyboard = document.querySelector('.piano__keyboard');
 let controls = document.querySelectorAll('.piano__control__option');
 let pianoLetters = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+let playBtn = document.querySelector('.piano__play-btn');
+let tempoSelect = document.querySelector('.piano__tempo');
+let songSelect = document.querySelector('.piano__song-list');
 let keys = [];
 let keyboardMap = [
   '1',
@@ -41,6 +44,62 @@ let keyboardMap = [
 ];
 let whiteButtons = [];
 let blackButtons = [];
+
+let jingleBells = `B3,,B3,,B3,,,,B3,,B3,,B3,,,,
+                       B3,,D4,,G3,,A3,B3,,,,,,
+                       C4,,C4,,C4,,,,C4,C4,,B3,,B3,,,,
+                       B3,B3,B3,,A3,,A3,,B3,,A3,,,,D4`;
+let happyBirthday = `G4,G4,A4,,G4,,C5,,B4,,,,
+                      G4,G4,A4,,G4,,D5,,C5,,,,
+                      G4,G4,G5,,E5,,C5,,B4,,A4,,
+                      F5,F5,E5,,C5,,D5,,C5,,,,`;
+
+let playSong = (notesString, tempo, cb) => {
+  let notes = notesString.split(',');
+  let currentNote = 0;
+  let mousedown = new Event('mousedown');
+  let mouseup = new Event('mouseup');
+  let btn;
+
+  let interval = setInterval(() => {
+    if (currentNote < notes.length) {
+      if (currentNote < notes.length) {
+        if (notes[currentNote].trim() !== '') {
+          if (btn) {
+            btn.dispatchEvent(mouseup);
+          }
+          btn = document.querySelector(
+            `[data-letter-note="${notes[currentNote].trim()}"]`
+          );
+          btn.dispatchEvent(mousedown);
+        }
+        currentNote++;
+      }
+    } else {
+      btn.dispatchEvent(mouseup);
+      clearInterval(interval);
+      cb();
+    }
+  }, 300 / tempo);
+};
+
+playBtn.addEventListener('mousedown', () => {
+  let tempo = +tempoSelect.value;
+  let songNum = +songSelect.value;
+  playBtn.disabled = true;
+
+  let enablePlayBtn = () => (playBtn.disabled = false);
+
+  switch (songNum) {
+    case 1:
+      playSong(jingleBells, tempo, enablePlayBtn);
+      break;
+
+    case 2:
+      playSong(happyBirthday, tempo, enablePlayBtn);
+      break;
+  }
+});
 
 let init = () => {
   for (let i = 1; i <= 5; i++) {
